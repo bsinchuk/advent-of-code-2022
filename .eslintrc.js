5a5eec15c05a58f0ea02b5015cbb1ec86c15e745
Bogdan Sinchuk <bogdan.sinchuk@insightsoftware.com>
2022-12-02 10:42:51 +0200

    Setting up the repo

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'metarhia',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
